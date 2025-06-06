039b0142482f875b3596f5d9ed326b899f6a32dd
royalbear37 <a905014737@gmail.com>
2025-06-07 05:06:06 +0800

    b

---
+++ config/mysql_connect.sample.php
@@ -0,0 +1,11 @@
+<?php
+$host = "localhost";
+$username = "root";
+$password = "自己的密碼";  // 如果沒有密碼就留空字串 ""
+$database = "clinic_db";
+
+$conn = new mysqli($host, $username, $password, $database);
+if ($conn->connect_error) {
+    die("資料庫連線失敗：" . $conn->connect_error);
+}
+?>
